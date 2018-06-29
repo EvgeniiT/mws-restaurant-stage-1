7fb19b114cf52f3a019a5b1a3d30f4e3a1d4c027
Evgenii Tkachev <3363867@gmail.com>
2018-06-29 17:08:36 +0300

    fix: url of sw and /

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var markers = [];
   return;
   }
 
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('./sw.js')
   .then( () => console.log('Service Worker Registered') )
   .catch(error => console.log('Registration failed: ', error));
  })();
